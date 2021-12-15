1a685700fe1bc67e87efea5c229dbb4891b43e2b
duruyao <duruyao@vimicro.com>
2021-12-15 17:00:16 +0800

    build: update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# date:   2021.11.16
-# author: duruyao@gmail.com
-# desc:   cross compile GoFS for multi-platform
+## date:   2021.11.16
+## author: duruyao@gmail.com
+## desc:   cross compile GoFS for multi-platform
 
 releases=(
   'GoFS-macOS-amd64 darwin amd64'
@@ -11,7 +11,8 @@ releases=(
   'GoFS-Linux-amd64 linux amd64'
   'GoFS-Windows-386.exe windows 386'
   'GoFS-Windows-arm.exe windows arm'
-  'GoFS-Windows-amd64.exe windows amd64')
+  'GoFS-Windows-amd64.exe windows amd64'
+)
 
 echo "GOROOT=${GOROOT}"
 echo "GOPATH=${GOPATH}"
@@ -21,15 +22,12 @@ echo
 
 releases_dir="$(pwd)/releases"
 
-mkdir -p "${releases_dir}"
+mkdir -p "${releases_dir}" && rm -rf "${releases_dir}"/GoFS*
 
 for release in "${releases[@]}"; do
-  items=(${release}) # NOTE: (${release}) != ("${release}")
-  #  echo "${items[*]}"
-  #  echo "0: ${items[0]}"
-  #  echo "1: ${items[1]}"
-  #  echo "2: ${items[2]}"
+  items=( ${release} ) ## NOTE: ( ${release} ) != ( "${release}" )
   cmd="GO_ENABLED=0 GOOS=${items[1]} GOARCH=${items[2]} ${GOEXEC} build -o ${releases_dir}/${items[0]} github.com/duruyao/gofs"
   echo "${cmd}"
   GO_ENABLED=0 GOOS=${items[1]} GOARCH=${items[2]} ${GOEXEC} build -o "${releases_dir}"/"${items[0]}" github.com/duruyao/gofs
 done
+
