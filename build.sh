da16cbc42b4d2a0bc0ef7c737b1c830c677cac1c
duruyao <duruyao@gmail.com>
2022-05-18 17:48:01 +0800

    build: update build.sh

--- build.sh
+++ build.sh
@@ -2,57 +2,85 @@
 
 ## date:   2021.11.16
 ## author: duruyao@gmail.com
-## desc:   cross compile GoShare for multi-platform
-## usage:  bash build.sh [all]
+## desc:   cross compile go application for multi-platform
+## usage:  bash build.sh [--all]
 
 set -euo pipefail
 
+function error_ln() {
+  # usage: error_ln "error message"
+  printf "\033[1;32;31m%s\n\033[m" "${1}"
+}
+
 release_list=(
-  'GoShare-macOS-amd64 darwin amd64 goshare'
-  'GoShare-Linux-386 linux 386 goshare'
-  'GoShare-Linux-arm linux arm goshare'
-  'GoShare-Linux-amd64 linux amd64 goshare'
-  'GoShare-Windows-386 windows 386 goshare.exe'
-  'GoShare-Windows-arm windows arm goshare.exe'
-  'GoShare-Windows-amd64 windows amd64 goshare.exe'
+  "darwin 386"
+  "darwin amd64"
+  "darwin arm"
+  "darwin arm64"
+  "linux 386"
+  "linux amd64"
+  "linux arm"
+  "linux arm64"
+  "windows 386"
+  "windows amd64"
+  "windows arm"
+  "windows arm64"
 )
 
-package="github.com/duruyao/goshare"
+if ! [ -f "go.mod" ]; then
+  error_ln "Error: 'go.mod' required but not found" >&2
+  echo "Usage: $0 [--all]" >&2
+  exit 1
+fi
+package="$(grep "module.*" go.mod | sed "s/module//g" | sed "s/ //g")"
+app="$(basename "${package}")"
 
 ## compile goshare for current platform
-if [ -z "$1" ] || [ "$1" != "all" ]; then
-  bash -x -c "GOROOT=${GOROOT} GOPATH=${GOPATH} GO_ENABLED=0 ${GOROOT}/bin/go build -o ${PWD}/goshare ${package}"
-  exit
+if [ $# == 0 ]; then
+  bash -x -c "GOROOT=${GOROOT} GOPATH=${GOPATH} GO_ENABLED=0 ${GOROOT}/bin/go build -o ${PWD}/${app} ${package}"
+  exit 0
+fi
+if [ $# != 0 ] && [ "$1" != "--all" ]; then
+  error_ln "Error: Unknown flags" >&2
+  echo "Usage: $0 [--all]" >&2
+  exit 1
 fi
 
 ## cross compile goshare for multi-platform
 releases_dir="${PWD}/releases"
-version="$(grep -o "version.*" "${PWD}"/package.json | grep -o "[0-9]\+.[0-9]\+.[0-9]\+")"
+if ! [ -f "package.json" ]; then
+  error_ln "Error: 'package.json' required but not found" >&2
+  echo "Usage: $0 [--all]" >&2
+  exit 1
+fi
+version="$(grep -o "version.*" package.json | head -1 | grep -o "[0-9]\+.[0-9]\+.[0-9]\+")"
 
 mkdir -p "${releases_dir}"
 rm -rf "${releases_dir:?}"/*
 pushd "${releases_dir}" 1>/dev/null 2>&1
 
 for release in "${release_list[@]}"; do
+  echo ""
   # shellcheck disable=SC2206
-  release=(${release})
-  deploy_dir="${PWD}/${release[0]}-${version}"
-  mkdir -p "${deploy_dir}"
+  release=(${release[*]})
+  target_dir="${PWD}/${app}-${release[0]}-${release[1]}-${version}"
+  target_path="${target_dir}/${app}"
+  if [ "${release[0]}" == "windows" ]; then
+    target_path="${target_dir}/${app}.exe"
+  fi
 
-  bash -x -c "GOROOT=${GOROOT} GOPATH=${GOPATH} GO_ENABLED=0 GOOS=${release[1]} GOARCH=${release[2]} ${GOROOT}/bin/go build -o ${deploy_dir}/${release[3]} ${package}"
-  chmod +x "${deploy_dir}/${release[3]}"
+  mkdir -p "${target_dir}"
+  bash -x -c "GOROOT=${GOROOT} GOPATH=${GOPATH} GO_ENABLED=0 GOOS=${release[0]} GOARCH=${release[1]} ${GOROOT}/bin/go build -o ${target_path} ${package}" || continue
+  chmod +x "${target_path}"
 
   if [ -n "$(command -v zip)" ]; then
-    bash -x -c "zip -r ${deploy_dir}.zip $(basename "${deploy_dir}") 1>/dev/null"
+    bash -x -c "zip -r ${target_dir}.zip $(basename "${target_dir}") 1>/dev/null"
   fi
-
   if [ -n "$(command -v tar)" ]; then
-    bash -x -c "tar -cvf ${deploy_dir}.tar $(basename "${deploy_dir}") 1>/dev/null"
-    bash -x -c "tar -zcvf ${deploy_dir}.tar.gz $(basename "${deploy_dir}") 1>/dev/null"
-    bash -x -c "tar -jcvf ${deploy_dir}.tar.bz2 $(basename "${deploy_dir}") 1>/dev/null"
+    bash -x -c "tar -cvf ${target_dir}.tar $(basename "${target_dir}") 1>/dev/null"
+    bash -x -c "tar -zcvf ${target_dir}.tar.gz $(basename "${target_dir}") 1>/dev/null"
+    bash -x -c "tar -jcvf ${target_dir}.tar.bz2 $(basename "${target_dir}") 1>/dev/null"
   fi
-
-  echo ""
 done
 
 popd 1>/dev/null 2>&1
