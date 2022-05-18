87b378b2fb7e07bfdbe19b1763602a25a55c6c46
duruyao <duruyao@gmail.com>
2022-05-18 14:25:45 +0800

    build: update build.sh

--- build.sh
+++ build.sh
@@ -3,62 +3,56 @@
 ## date:   2021.11.16
 ## author: duruyao@gmail.com
 ## desc:   cross compile GoShare for multi-platform
+## usage:  bash build.sh [all]
 
-releases=(
-  'goshare-macos-amd64 darwin amd64 GoShare-macOS-amd64 goshare'
-  'goshare-linux-386 linux 386 GoShare-Linux-386 goshare'
-  'goshare-linux-arm linux arm GoShare-Linux-arm goshare'
-  'goshare-linux-amd64 linux amd64 GoShare-Linux-amd64 goshare'
-  'goshare-windows-386.exe windows 386 GoShare-Windows-386 goshare.exe'
-  'goshare-windows-arm.exe windows arm GoShare-Windows-arm goshare.exe'
-  'goshare-windows-amd64.exe windows amd64 GoShare-Windows-amd64 goshare.exe'
+set -euo pipefail
+
+release_list=(
+  'GoShare-macOS-amd64 darwin amd64 goshare'
+  'GoShare-Linux-386 linux 386 goshare'
+  'GoShare-Linux-arm linux arm goshare'
+  'GoShare-Linux-amd64 linux amd64 goshare'
+  'GoShare-Windows-386 windows 386 goshare.exe'
+  'GoShare-Windows-arm windows arm goshare.exe'
+  'GoShare-Windows-amd64 windows amd64 goshare.exe'
 )
 
-echo "GOROOT=${GOROOT}"
-echo "GOPATH=${GOPATH}"
-GOEXEC="$(command -v go)"
-echo "GOEXEC=${GOEXEC}"
-echo
+package="github.com/duruyao/goshare"
 
 ## compile goshare for current platform
 if [ -z "$1" ] || [ "$1" != "all" ]; then
-  echo "GO_ENABLED=0 ${GOEXEC} build github.com/duruyao/goshare"
-  GO_ENABLED=0 ${GOEXEC} build github.com/duruyao/goshare
+  bash -x -c "GOROOT=${GOROOT} GOPATH=${GOPATH} GO_ENABLED=0 ${GOROOT}/bin/go build -o ${PWD}/goshare ${package}"
   exit
 fi
 
 ## cross compile goshare for multi-platform
 releases_dir="${PWD}/releases"
-version_id="$(TZ=UTC-8 date '+%Y.%m.%d')"
-if [ -n "$2" ]; then
-  version_id="$2"
-fi
+version="$(grep -o "version.*" "${PWD}"/package.json | grep -o "[0-9]\+.[0-9]\+.[0-9]\+")"
 
-mkdir -p "${releases_dir}" && rm -rf "${releases_dir:?}"/*
+mkdir -p "${releases_dir}"
+rm -rf "${releases_dir:?}"/*
+pushd "${releases_dir}" 1>/dev/null 2>&1
 
-pushd "${releases_dir}" || exit
-
-for release in "${releases[@]}"; do
-  items=(${release}) ## NOTE: ( ${release} ) != ( "${release}" )
-  cmd="GO_ENABLED=0 GOOS=${items[1]} GOARCH=${items[2]} ${GOEXEC} build -o ${releases_dir}/${items[0]} github.com/duruyao/goshare"
-  echo "${cmd}"
-  GO_ENABLED=0 GOOS=${items[1]} GOARCH=${items[2]} ${GOEXEC} build -o "${releases_dir}"/"${items[0]}" github.com/duruyao/goshare
-  deploy_dir="${releases_dir}/${items[3]}-${version_id}-release"
-
+for release in "${release_list[@]}"; do
+  # shellcheck disable=SC2206
+  release=(${release})
+  deploy_dir="${PWD}/${release[0]}-${version}"
   mkdir -p "${deploy_dir}"
-  cp "${releases_dir}/${items[0]}" "${deploy_dir}/${items[4]}"
+
+  bash -x -c "GOROOT=${GOROOT} GOPATH=${GOPATH} GO_ENABLED=0 GOOS=${release[1]} GOARCH=${release[2]} ${GOROOT}/bin/go build -o ${deploy_dir}/${release[3]} ${package}"
+  chmod +x "${deploy_dir}/${release[3]}"
 
   if [ -n "$(command -v zip)" ]; then
-    zip -r "${deploy_dir}.zip" "$(basename "${deploy_dir}")"
+    bash -x -c "zip -r ${deploy_dir}.zip $(basename "${deploy_dir}") 1>/dev/null"
   fi
 
   if [ -n "$(command -v tar)" ]; then
-    tar -cvf "${deploy_dir}.tar" "$(basename "${deploy_dir}")"
-    tar -zcvf "${deploy_dir}.tar.gz" "$(basename "${deploy_dir}")"
-    tar -jcvf "${deploy_dir}.tar.bz2" "$(basename "${deploy_dir}")"
+    bash -x -c "tar -cvf ${deploy_dir}.tar $(basename "${deploy_dir}") 1>/dev/null"
+    bash -x -c "tar -zcvf ${deploy_dir}.tar.gz $(basename "${deploy_dir}") 1>/dev/null"
+    bash -x -c "tar -jcvf ${deploy_dir}.tar.bz2 $(basename "${deploy_dir}") 1>/dev/null"
   fi
 
-  rm -r "${deploy_dir}" || exit
+  echo ""
 done
 
-popd || exit
+popd 1>/dev/null 2>&1
