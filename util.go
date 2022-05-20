0f94674df339e7cf3872eff18781992c756a0ace
duruyao <duruyao@gmail.com>
2022-05-20 15:13:03 +0800

    refactor: add the function `CurrentDirMust`

--- util.go
+++ util.go
@@ -7,11 +7,6 @@ import (
 	"sync"
 )
 
-var (
-	userHomeDirOnce sync.Once
-	userHomeDir     string
-)
-
 func AbsPathMust(path string) string {
 	abs, err := filepath.Abs(path)
 	if err != nil {
@@ -20,7 +15,12 @@ func AbsPathMust(path string) string {
 	return abs
 }
 
-// UserHomeDirMust returns the home directory of the current user, such as "/home/user" in Unix-like OS.
+var (
+	userHomeDirOnce sync.Once
+	userHomeDir     string
+)
+
+// UserHomeDirMust returns the home directory of the current user, such as "/home/foo" in Unix-like OS.
 func UserHomeDirMust() string {
 	userHomeDirOnce.Do(func() {
 		var err error
@@ -30,4 +30,21 @@ func UserHomeDirMust() string {
 		}
 	})
 	return userHomeDir
+}
+
+var (
+	currentDirOnce sync.Once
+	currentDir     string
+)
+
+// CurrentDirMust returns the current directory.
+func CurrentDirMust() string {
+	currentDirOnce.Do(func() {
+		var err error
+		currentDir, err = os.Getwd()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	})
+	return currentDir
 }
