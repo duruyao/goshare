f605e1687a9482743f43cc17c4f11945b2d0b99c
duruyao <duruyao@gmail.com>
2022-05-23 20:09:56 +0800

    refactor: refactor the file `args.go`

--- args.go
+++ args.go
@@ -1,146 +1,150 @@
 package main
 
 import (
-	"errors"
+	"bytes"
+	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
+	"sync"
 	"text/template"
 )
 
-var (
-	WantVersion   bool
-	Scheme        string
-	UrlPrefix     string
-	HandlingPath  string
-	ListeningAddr string
-)
-
 const (
-	SH            = ` (shorthand)`
-	VersionSerial = `GoShare Version 2021.12.22`
-	UsageTmpl     = `Usage: {{.App}} [OPTIONS] PATH
+	DefaultHost      = `localhost:3927`
+	DefaultScheme    = `http`
+	DefaultUrlPrefix = `/`
+	UsageTmpl        = `Usage: {{.Exec}} [OPTIONS]
 
-The tool share files or directories by HTTP or FTP protocol
+GoShare shares file and directory by HTTP or FTP protocol
 
 Options:
-	-d, --detach				Run service in background
-    -h, --help      			Display this help message
-    --host STRING          		Host address to listen (default: '{{.DefaultAddr}}')
-    --scheme STRING				Scheme name (default: '{{.DefaultScheme}}')
-    --url-prefix STRING 		URL prefix (default: 'PATH')
-    -v, --version   			Print version information and quit
+    -h, --help                  Display this help message
+    --host STRING               Host address to listen (format: 'IP:PORT') (default: '{{.Host}}')
+    --path STRING               Path or directory (default: '{{.Path}}')
+    --scheme STRING             Scheme name (default: '{{.Scheme}}')
+    --url-prefix STRING         Custom URL prefix (default: '{{.UrlPrefix}}')
+    -v, --version               Print version information and quit
 
 Examples:
-    {{.App}} -host example.com /opt/share0/releases/
-    {{.App}} -host localhost:3927 /opt/share0/releases/
-    {{.App}} --host localhost:3927 --url-prefix /releases/ /opt/share0/releases/
-    {{.App}} --host=localhost:3927 --url-prefix=/releases/ /opt/share0/releases/
+    {{.Exec}} -host example.io -path /opt/share0/releases/
+    {{.Exec}} -host {{.Host}} -path /opt/share0/releases/
+    {{.Exec}} --host {{.Host}} --url-prefix /releases/ --path /opt/share0/releases/
+    {{.Exec}} --host={{.Host}} --url-prefix=/releases/ --path=/opt/share0/releases/
+
+See more about {{.App}} at {{.AppLink}}
 `
 )
 
-func WantUsage() bool {
-	helpArgs := map[string]bool{
-		"-h":     true,
-		"--h":    true,
-		"-help":  true,
-		"--help": true,
-	}
-	for _, arg := range os.Args[1:] {
-		if helpArgs[arg] {
-			return true
-		}
-	}
-	return false
+var (
+	DefaultPath = CurrentDirMust()
+)
+
+type argument struct {
+	WantHelp    bool   `json:"want_help"`
+	Host        string `json:"host"`
+	Path        string `json:"path"`
+	Scheme      string `json:"scheme"`
+	UrlPrefix   string `json:"url_prefix"`
+	WantVersion bool   `json:"want_version"`
 }
 
-func ShowUsage() {
-	tmpl := template.Must(template.New("usage tmpl").Parse(UsageTmpl))
+type Argument struct {
+	arg       *argument
+	parseOnce sync.Once
+}
 
-	data := struct {
-		App           string
-		DefaultScheme string
-		DefaultAddr   string
-		DefaultPath   string
-	}{
-		App:           os.Args[0],
-		DefaultPath:   UserHomeDirMust(),
-		DefaultScheme: "http",
-		DefaultAddr:   "localhost:3927",
+func NewArgument() *Argument {
+	return &Argument{arg: &argument{}}
+}
+
+func (a *Argument) Init() {
+	flag.BoolVar(&(a.arg.WantHelp), "h", false, "")
+	flag.BoolVar(&(a.arg.WantHelp), "help", false, "")
+	flag.StringVar(&(a.arg.Host), "host", DefaultHost, "")
+	flag.StringVar(&(a.arg.Path), "path", DefaultPath, "")
+	flag.StringVar(&(a.arg.Scheme), "scheme", DefaultScheme, "")
+	flag.StringVar(&(a.arg.UrlPrefix), "url-prefix", DefaultUrlPrefix, "")
+	flag.BoolVar(&(a.arg.WantVersion), "v", false, "")
+	flag.BoolVar(&(a.arg.WantVersion), "version", false, "")
+}
+
+func (a *Argument) Parse() {
+	a.parseOnce.Do(func() {
+		flag.Parse()
+	})
+}
+
+func (a *Argument) Serialize() ([]byte, error) {
+	return json.Marshal(a.arg)
+}
+
+func (a *Argument) Deserialize(js []byte) error {
+	return json.Unmarshal(js, a.arg)
+}
+
+func (a *Argument) String() string {
+	a.Parse()
+	if js, err := json.MarshalIndent(a.arg, "", "    "); err != nil {
+		return err.Error()
+	} else {
+		return string(js)
 	}
+}
 
-	if err := tmpl.Execute(os.Stdout, data); err != nil {
+func (a *Argument) WantHelp() bool {
+	a.Parse()
+	return a.arg.WantHelp
+}
+
+func (a *Argument) Host() string {
+	a.Parse()
+	return a.arg.Host
+}
+
+func (a *Argument) Path() string {
+	a.Parse()
+	return a.arg.Path
+}
+
+func (a *Argument) Scheme() string {
+	a.Parse()
+	return a.arg.Scheme
+}
+
+func (a *Argument) UrlPrefix() string {
+	a.Parse()
+	return a.arg.UrlPrefix
+}
+
+func (a *Argument) WantVersion() bool {
+	a.Parse()
+	return a.arg.WantVersion
+}
+
+func (a *Argument) Usage() string {
+	a.Parse()
+	tmpl := template.Must(template.New("usage tmpl").Parse(UsageTmpl))
+	data := struct {
+		App       string
+		AppLink   string
+		Exec      string
+		Host      string
+		Path      string
+		Scheme    string
+		UrlPrefix string
+	}{
+		App:       App,
+		AppLink:   AppLink,
+		Exec:      os.Args[0],
+		Host:      DefaultHost,
+		Path:      DefaultPath,
+		Scheme:    DefaultScheme,
+		UrlPrefix: DefaultUrlPrefix,
+	}
+	buf := bytes.Buffer{}
+	if err := tmpl.Execute(&buf, data); err != nil {
 		log.Fatalln(err)
 	}
-}
-
-func ShowVersion() {
-	fmt.Println(VersionSerial)
-}
-
-func InitArgs() {
-	flag.StringVar(&UrlPrefix, "url-prefix", "", "url prefix")
-
-	flag.StringVar(&Scheme, "s", "http", "scheme name"+SH)
-	flag.StringVar(&Scheme, "scheme", "http", "scheme name")
-
-	flag.BoolVar(&WantVersion, "v", false, "show version"+SH)
-	flag.BoolVar(&WantVersion, "version", false, "show version")
-
-	flag.StringVar(&HandlingPath, "p", UserHomeDirMust(), "handing path or directory"+SH)
-	flag.StringVar(&HandlingPath, "path", UserHomeDirMust(), "handing path or directory")
-
-	flag.StringVar(&ListeningAddr, "a", "127.0.0.1:8080", "listening address"+SH)
-	flag.StringVar(&ListeningAddr, "address", "127.0.0.1:8080", "listening address")
-}
-
-// ParseArgs parses some values from arguments.
-func ParseArgs() (addr string, dir string, filename string, prefix string, url string, err error) {
-	if WantUsage() {
-		ShowUsage()
-		err = errors.New("just show usage")
-		return
-	}
-
-	flag.Parse()
-	if WantVersion {
-		ShowVersion()
-		err = errors.New("just show version")
-		return
-	}
-
-	info, e := os.Stat(HandlingPath)
-	if os.IsNotExist(err) {
-		fmt.Printf("No such file or directory: %s\n", HandlingPath)
-		err = e
-		return
-	}
-
-	addr = ListeningAddr // NOTE: set addr
-	HandlingPath = AbsPathMust(HandlingPath)
-	if info.IsDir() {
-		dir = HandlingPath
-		filename = ""
-	} else {
-		dir = filepath.Dir(HandlingPath)       // NOTE: set dir
-		filename = filepath.Base(HandlingPath) // NOTE: set filename
-	}
-
-	if UrlPrefix != "" { // FIXME: support url prefix = /
-		if UrlPrefix[0] == '/' {
-			UrlPrefix = UrlPrefix[1:]
-		}
-		if UrlPrefix[len(UrlPrefix)-1] == '/' {
-			UrlPrefix = UrlPrefix[:len(UrlPrefix)-1]
-		}
-		prefix = UrlPrefix
-	} else {
-		prefix = filepath.Base(dir) // NOTE: set prefix
-	}
-
-	url = fmt.Sprintf("%s://%s/%s/%s", "http", addr, prefix, filename) // NOTE: set url
-
-	return
+	return buf.String()
 }
