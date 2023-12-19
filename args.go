e2650030e2f5b35a8d45e5cf44f3accd5c3b6ffa
Ryan Du <duruyao@gmail.com>
2023-12-19 16:51:12 +0800

    docs: update LICENSE

--- args.go
+++ args.go
@@ -1,4 +1,4 @@
-//  Copyright (c) 2022, DURUYAO. All rights reserved.
+//  Copyright 2022-2032 Ryan Du <duruyao@gmail.com>
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
